0dced9da3e2280c22a6e1d6549ab29ac5de11dc8
Phil Carns <carns@mcs.anl.gov>
2021-12-06 17:51:46 -0500

    stub in bedrock module

---
+++ include/quintain-server.h
@@ -0,0 +1,104 @@
+/*
+ * (C) 2021 The University of Chicago
+ *
+ * See COPYRIGHT in top-level directory.
+ */
+
+#ifndef __QUINTAIN_SERVER_H
+#define __QUINTAIN_SERVER_H
+
+#include <margo.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define QUINTAIN_PROVIDER_ID_DEFAULT 0
+#define QUINTAIN_PROVIDER_IGNORE     NULL
+
+typedef struct quintain_provider* quintain_provider_t;
+
+#define QTN_TRACE(_mid, _format, f...) \
+    margo_trace(_mid, "quintain: " _format, ##f)
+#define QTN_DEBUG(_mid, _format, f...) \
+    margo_debug(_mid, "quintain: " _format, ##f)
+#define QTN_INFO(_mid, _format, f...) \
+    margo_info(_mid, "quintain: " _format, ##f)
+#define QTN_WARNING(_mid, _format, f...) \
+    margo_warning(_mid, "quintain: " _format, ##f)
+#define QTN_ERROR(_mid, _format, f...) \
+    margo_error(_mid, "quintain: " _format, ##f)
+#define QTN_CRITICAL(_mid, _format, f...) \
+    margo_critical(_mid, "quintain: " _format, ##f)
+
+/**
+ * The quintain_provider_init_info structure can be passed in to the
+ * quintain_provider_register() function to configure the provider. The struct
+ * can be memset to zero to use default values.
+ */
+struct quintain_provider_init_info {
+    const char* json_config; /* optional JSON-formatted string */
+    ABT_pool    rpc_pool;    /* optional pool on which to run RPC handlers */
+};
+
+/**
+ * Example JSON configuration:
+ * ----------------------------------------------
+{
+}
+ * ----------------------------------------------
+ */
+
+#define QUINTAIN_PROVIDER_INIT_INFO_INITIALIZER \
+    {                                           \
+        NULL, ABT_POOL_NULL                     \
+    }
+
+/**
+ * Initializes a QUINTAIN provider.
+ *
+ * @param[in] provider_id provider id
+ * @param[in] args initialization parameters
+ * @param[out] provider resulting provider
+ *
+ * @returns 0 on success, -1 otherwise
+ */
+int quintain_provider_register(margo_instance_id mid,
+                               uint16_t          provider_id,
+                               const struct quintain_provider_init_info* args,
+                               quintain_provider_t* provider);
+
+/**
+ * @brief Deregisters the provider.
+ *
+ * @param provider Provider to deregister.
+ *
+ * @return 0 on success, -1 otherwise.
+ */
+int quintain_provider_deregister(quintain_provider_t provider);
+
+/**
+ * Sets configuration parameters
+ *
+ * @param [in] provider Bake provider
+ * @param [in] key parameter name
+ * @param [in] value parameter value
+ *
+ * @returns 0 on success, -1 on failure
+ */
+int quintain_provider_set_param(quintain_provider_t provider,
+                                const char*         key,
+                                const char*         value);
+/**
+ * Retrieves complete configuration of quintain provider, encoded as json
+ *
+ * @param [in] provider quintain provider
+ * @returns null terminated string that must be free'd by caller
+ */
+char* quintain_provider_get_config(quintain_provider_t provider);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __QUINTAIN_SERVER_H */
---
+++ src/quintain-bedrock-module.c
@@ -0,0 +1,118 @@
+/*
+ * (C) 2021 The University of Chicago
+ *
+ * See COPYRIGHT in top-level directory.
+ */
+
+#include <string.h>
+#include <bedrock/module.h>
+#include <quintain-server.h>
+
+static int quintain_register_provider(bedrock_args_t             args,
+                                      bedrock_module_provider_t* provider)
+{
+    int                                ret;
+    struct quintain_provider_init_info bpargs = {0};
+    margo_instance_id mid         = bedrock_args_get_margo_instance(args);
+    uint16_t          provider_id = bedrock_args_get_provider_id(args);
+    ABT_pool          pool        = bedrock_args_get_pool(args);
+    const char*       config      = bedrock_args_get_config(args);
+    const char*       name        = bedrock_args_get_name(args);
+
+    QTN_TRACE(mid, "quintain_register_provider()");
+    QTN_TRACE(mid, " -> mid           = %p", (void*)mid);
+    QTN_TRACE(mid, " -> provider id   = %d", provider_id);
+    QTN_TRACE(mid, " -> pool          = %p", (void*)pool);
+    QTN_TRACE(mid, " -> config        = %s", config);
+    QTN_TRACE(mid, " -> name          = %s", name);
+
+    bpargs.json_config = config;
+    ret                = quintain_provider_register(mid, provider_id, &bpargs,
+                                                    (quintain_provider_t*)provider);
+    if (ret < 0) return (-1);
+
+    return BEDROCK_SUCCESS;
+}
+
+static int quintain_deregister_provider(bedrock_module_provider_t provider)
+{
+    int ret;
+
+    ret = quintain_provider_deregister(provider);
+    if (ret < 0) return (-1);
+
+    return BEDROCK_SUCCESS;
+}
+
+static char* quintain_get_provider_config(bedrock_module_provider_t provider)
+{
+    return (quintain_provider_get_config(provider));
+}
+
+static int quintain_init_client(bedrock_args_t           args,
+                                bedrock_module_client_t* client)
+{
+    int ret;
+
+    margo_instance_id mid = bedrock_args_get_margo_instance(args);
+    QTN_TRACE(mid, "quintain_init_client()");
+
+    /* TODO: fill this in */
+
+    return BEDROCK_SUCCESS;
+}
+
+static int quintain_finalize_client(bedrock_module_client_t client)
+{
+    int ret;
+
+    /* TODO: fill this in */
+
+    return BEDROCK_SUCCESS;
+}
+
+static char* quintain_get_client_config(bedrock_module_client_t client)
+{
+    return strdup("{}");
+}
+
+static int quintain_create_provider_handle(bedrock_module_client_t client,
+                                           hg_addr_t               address,
+                                           uint16_t                provider_id,
+                                           bedrock_module_provider_handle_t* ph)
+{
+    int ret;
+
+    /* TODO: fill this in */
+
+    return BEDROCK_SUCCESS;
+}
+
+static int quintain_destroy_provider_handle(bedrock_module_provider_handle_t ph)
+{
+    int ret;
+
+    /* TODO: fill this in */
+
+    return BEDROCK_SUCCESS;
+}
+
+struct bedrock_dependency quintain_provider_deps[1]
+    = {BEDROCK_NO_MORE_DEPENDENCIES};
+
+struct bedrock_dependency quintain_client_deps[1]
+    = {BEDROCK_NO_MORE_DEPENDENCIES};
+
+static struct bedrock_module quintain
+    = {.register_provider       = quintain_register_provider,
+       .deregister_provider     = quintain_deregister_provider,
+       .get_provider_config     = quintain_get_provider_config,
+       .init_client             = quintain_init_client,
+       .finalize_client         = quintain_finalize_client,
+       .get_client_config       = quintain_get_client_config,
+       .create_provider_handle  = quintain_create_provider_handle,
+       .destroy_provider_handle = quintain_destroy_provider_handle,
+       .provider_dependencies   = quintain_provider_deps,
+       .client_dependencies     = quintain_client_deps};
+
+BEDROCK_REGISTER_MODULE(quintain, quintain)
