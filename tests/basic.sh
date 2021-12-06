9401b042979e54e9db55042ec0141756ff10a360
Phil Carns <carns@mcs.anl.gov>
2021-12-06 16:54:56 -0500

    add testcase with placeholder bedrock daemon start

---
+++ tests/basic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [ -z $srcdir ]; then
+    echo srcdir variable not set.
+    exit 1
+fi
+
+bedrock -c $srcdir/tests/mochi-quintain-provider.yaml na+sm:// &
+BEDROCK_PID=$!
+
+sleep 2
+
+kill $BEDROCK_PID
+wait
