c85aceb42c2a873830f52208cc9556ed3753f9aa
Phil Carns <carns@mcs.anl.gov>
2021-12-21 15:13:40 -0500

    record nranks in benchmark json

--- src/quintain-benchmark.c
+++ src/quintain-benchmark.c
@@ -17,6 +17,23 @@
 #include <ssg.h>
 #include <quintain-client.h>
 
+// Overrides a field with an integer. If the field already existed and was
+// different from the new value, and __warning is true, prints a warning.
+#define CONFIG_OVERRIDE_INTEGER(__config, __key, __value, __warning)          \
+    do {                                                                      \
+        struct json_object* _tmp = json_object_object_get(__config, __key);   \
+        if (_tmp && __warning) {                                              \
+            if (!json_object_is_type(_tmp, json_type_int))                    \
+                fprintf(stderr, "Overriding field \"%s\" with value %d",      \
+                        __key, (int)__value);                                 \
+            else if (json_object_get_int(_tmp) != __value)                    \
+                fprintf(stderr, "Overriding field \"%s\" (%d) with value %d", \
+                        __key, json_object_get_int(_tmp), __value);           \
+        }                                                                     \
+        json_object_object_add(__config, __key,                               \
+                               json_object_new_int64(__value));               \
+    } while (0)
+
 struct options {
     char group_file[256];
     char json_file[256];
@@ -191,6 +208,7 @@ static int parse_json(const char* json_file, struct json_object** json_cfg)
     char*                   json_cfg_str = NULL;
     FILE*                   f;
     long                    fsize;
+    int                     nranks;
 
     /* open json file */
     f = fopen(json_file, "r");
@@ -228,6 +246,10 @@ static int parse_json(const char* json_file, struct json_object** json_cfg)
     }
     json_tokener_free(tokener);
     free(json_cfg_str);
+
+    /* validate input params or fill in defaults */
+    MPI_Comm_size(MPI_COMM_WORLD, &nranks);
+    CONFIG_OVERRIDE_INTEGER(*json_cfg, "nranks", nranks, 1);
 
     return (0);
 }
